cb91fdb0e487bc798f8b7d8d18182fce2d8a604f
Gabriel <gb9@alumnes.udl.cat>
2025-02-19 18:37:55 +0100

    Director extends User

--- src/main/java/cat/udl/eps/softarch/tfgfinder/domain/Director.java
+++ src/main/java/cat/udl/eps/softarch/tfgfinder/domain/Director.java
@@ -1,5 +1,13 @@
 package cat.udl.eps.softarch.tfgfinder.domain;
 
-public class Director extends User{
+import jakarta.persistence.Entity;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
 
+@Entity
+@Data
+@EqualsAndHashCode(callSuper = true)
+public class Director extends User{
+    private int numFinishedProposals;
+    private int numACtiveProposals;
 }
