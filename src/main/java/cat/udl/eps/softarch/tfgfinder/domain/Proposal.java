e35691aa3b10454dbaa34a60cff4ef27a2ed8eb3
David Carreras <davidcarreras2003@gmail.com>
2025-02-19 18:21:37 +0100

    first approach proposal class basic attributes

---
+++ src/main/java/cat/udl/eps/softarch/tfgfinder/domain/Proposal.java
@@ -0,0 +1,45 @@
+package cat.udl.eps.softarch.tfgfinder.domain;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+import java.util.List;
+
+@Entity
+@Table(name = "Proposal")
+@Data
+@EqualsAndHashCode(callSuper = true)
+public class Proposal extends UriEntity<Long> {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank
+    private String title;
+
+    @NotBlank
+    @Lob
+    private String description;
+
+    @NotBlank
+    private String timing;
+
+    @NotBlank
+    private String speciality;
+
+    @NotBlank
+    private String kind;
+
+    @NotBlank
+    @ElementCollection
+    private List<String> keywords;
+
+    @ManyToOne(optional = false)
+    @JoinColumn(name = "owner_id", nullable = false)
+    private User owner;
+
+}
