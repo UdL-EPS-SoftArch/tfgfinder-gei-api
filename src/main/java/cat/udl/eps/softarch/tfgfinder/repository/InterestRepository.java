93d25b18d9ed065d62e720ee2a9da46257dbc9ee
wissamamer <wissamamer2000@gmail.com>
2025-02-20 16:13:03 +0100

    Replace JpaRepository with PagingAndSortingRepository in InterestRepository

---
+++ src/main/java/cat/udl/eps/softarch/tfgfinder/repository/InterestRepository.java
@@ -0,0 +1,9 @@
+package cat.udl.eps.softarch.tfgfinder.repository;
+
+import cat.udl.eps.softarch.tfgfinder.domain.Interest;
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+@RepositoryRestResource
+public interface InterestRepository extends PagingAndSortingRepository<Interest, Long> {
+}
