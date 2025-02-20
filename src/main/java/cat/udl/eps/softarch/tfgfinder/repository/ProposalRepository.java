1e419337b7d74cd0fac6b1014483d23e475cdb92
Aleix-CB <aleix.cerqueda@gmail.com>
2025-02-20 16:08:09 +0100

    ProposalRepo

---
+++ src/main/java/cat/udl/eps/softarch/tfgfinder/repository/ProposalRepository.java
@@ -0,0 +1,25 @@
+package cat.udl.eps.softarch.tfgfinder.repository;
+
+import cat.udl.eps.softarch.tfgfinder.domain.Proposal;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+import java.util.List;
+
+@RepositoryRestResource
+public interface ProposalRepository extends CrudRepository<Proposal, String>, PagingAndSortingRepository<Proposal, String> {
+
+  /* Interface provides automatically, as defined in
+   * https://docs.spring.io/spring-data/commons/docs/current/api/org/springframework/data/repository/CrudRepository.html
+   * and
+   * https://docs.spring.io/spring-data/commons/docs/current/api/org/springframework/data/repository/PagingAndSortingRepository.html
+   * the methods: count, delete, deleteAll, deleteById, existsById, findAll, findAllById, findById, save, saveAll,...
+   *
+   * Additional methods like findByUsernameContaining can be defined following:
+   * https://docs.spring.io/spring-data/jpa/docs/current/reference/html/#jpa.query-methods.query-creation
+   */
+
+  List<Proposal> findByIdContaining(@Param("text") String text);
+}
