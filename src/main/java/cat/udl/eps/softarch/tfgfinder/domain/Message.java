0d8619944bb69edca964ed8ca91590232ec1b361
MarkGRX11 <marc11gandara@gmail.com>
2025-02-20 16:12:29 +0100

    Message Implementation

---
+++ src/main/java/cat/udl/eps/softarch/tfgfinder/domain/Message.java
@@ -0,0 +1,30 @@
+package cat.udl.eps.softarch.tfgfinder.domain;
+
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.Data;
+
+import java.time.ZonedDateTime;
+
+@Entity
+@Table(name = "Message") // Especifica que tindrà una taula pròpia
+@Data
+public class Message {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String id;
+
+    @NotBlank
+    private String text;
+
+    @NotNull
+    private ZonedDateTime when;
+
+    @ManyToOne
+    private User from;
+}
+
+
