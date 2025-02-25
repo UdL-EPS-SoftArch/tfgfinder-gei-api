efc5278ed4429779df807beb3d44fab91dc22a86
carless7 <csh3@alumnes.udl.cat>
2025-02-25 15:24:19 +0100

    feat: add JPA and Lombok annotations

--- src/main/java/cat/udl/eps/softarch/tfgfinder/domain/Professor.java
+++ src/main/java/cat/udl/eps/softarch/tfgfinder/domain/Professor.java
@@ -1,11 +1,17 @@
 package cat.udl.eps.softarch.tfgfinder.domain;
 
 import jakarta.persistence.ElementCollection;
+import jakarta.persistence.Entity;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.AuthorityUtils;
 
 import java.util.Collection;
 
+@Entity
+@Data
+@EqualsAndHashCode(callSuper = true)
 public class Professor extends Director {
     @Override
     @ElementCollection
