210aa3abbc29f0f5a7fa4b8ff6ec02b8f0350e45
carless7 <csh3@alumnes.udl.cat>
2025-02-25 15:17:31 +0100

    feat: class init

---
+++ src/main/java/cat/udl/eps/softarch/tfgfinder/domain/Professor.java
@@ -0,0 +1,11 @@
+package cat.udl.eps.softarch.tfgfinder.domain;
+
+import jakarta.persistence.ElementCollection;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.AuthorityUtils;
+
+import java.util.Collection;
+
+public class Professor extends Director {
+
+}
