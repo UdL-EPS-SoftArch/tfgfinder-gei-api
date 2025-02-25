59ab54466d7485b8663fdcb0c6843f63587af67a
Cynthia Ghannam <ghannamcynthia88@gmail.com>
2025-02-25 14:48:32 +0100

    Added InviteRepository

---
+++ src/main/java/cat/udl/eps/softarch/tfgfinder/repository/InviteRepository.java
@@ -0,0 +1,13 @@
+package cat.udl.eps.softarch.tfgfinder.repository;
+
+import cat.udl.eps.softarch.tfgfinder.domain.Invite;
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface InviteRepository extends JpaRepository<Invite, Long> {
+    // Custom query to find invites by status
+    List<Invite> findByStatus(String status);
+}
