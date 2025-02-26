2cc65ad3f79a07ccbe9870dae43351bd92ec0399
RaulFilip16 <maricicapasca@gmail.com>
2025-02-26 17:45:48 +0100

    "Chat" CLASS REIMPLEMENTED: I have removed the List of Messages and Proposal atributes

--- src/main/java/cat/udl/eps/softarch/tfgfinder/domain/Chat.java
+++ src/main/java/cat/udl/eps/softarch/tfgfinder/domain/Chat.java
@@ -1,10 +1,7 @@
 package cat.udl.eps.softarch.tfgfinder.domain;
 
 import jakarta.persistence.*;
-import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
-
-import java.util.List;
 
 @Entity
 @Data
@@ -13,12 +10,4 @@ public class Chat {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private String id;
-
-    @NotBlank
-    @OneToOne
-    private Proposal proposal;
-
-    @OneToMany(mappedBy = "chat", cascade = CascadeType.ALL, orphanRemoval = true)
-    private List<Message> messages;
-
 }
