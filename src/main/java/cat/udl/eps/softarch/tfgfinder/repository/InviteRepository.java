67199c3008ba5fd4ec09cd8a8a6cd5a57df2f846
wissamamer <wissamamer2000@gmail.com>
2025-02-26 18:04:12 +0100

    Add queries for InviteRepository and Interest

--- src/main/java/cat/udl/eps/softarch/tfgfinder/repository/InviteRepository.java
+++ src/main/java/cat/udl/eps/softarch/tfgfinder/repository/InviteRepository.java
@@ -1,22 +1,25 @@
 package cat.udl.eps.softarch.tfgfinder.repository;
 
-import cat.udl.eps.softarch.tfgfinder.domain.Interest;
-import cat.udl.eps.softarch.tfgfinder.domain.Proposal;
+
+import cat.udl.eps.softarch.tfgfinder.domain.Invite;
 import cat.udl.eps.softarch.tfgfinder.domain.User;
-import org.springframework.data.jpa.repository.Query;
+import cat.udl.eps.softarch.tfgfinder.domain.Proposal;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.PagingAndSortingRepository;
-import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
+
+
 @RepositoryRestResource
-public interface InterestRepository extends CrudRepository<Interest, Long>, PagingAndSortingRepository<Interest, Long> {
+public interface InviteRepository extends CrudRepository<Invite, Long>, PagingAndSortingRepository<Invite, Long> {
 
     @Query("SELECT i FROM Invite i WHERE i.who = :who")
-    List<Interest> findByWho(@Param("who") User who);
+    List<Invite> findByWho(@Param("who") User who);
     @Query("SELECT i FROM Invite i WHERE i.what = :what")
-    List<Interest> findByWhat(@Param("what") Proposal what);
+    List<Invite> findByWhat(@Param("what") Proposal what);
 
 }
