06cfdd73b87207be03980de67e569efe52a3c1a7
MarkGRX11 <marc11gandara@gmail.com>
2025-02-27 16:05:23 +0100

    FindByChat function implemented. And relationship between Message and Chat established.

--- src/main/java/cat/udl/eps/softarch/tfgfinder/repository/MessageRepository.java
+++ src/main/java/cat/udl/eps/softarch/tfgfinder/repository/MessageRepository.java
@@ -1,9 +1,13 @@
 package cat.udl.eps.softarch.tfgfinder.repository;
 
+import cat.udl.eps.softarch.tfgfinder.domain.Chat;
 import cat.udl.eps.softarch.tfgfinder.domain.Invite;
+import cat.udl.eps.softarch.tfgfinder.domain.Message;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.PagingAndSortingRepository;
 
-public interface MessageRepository extends CrudRepository<Invite, Long>, PagingAndSortingRepository<Invite, Long> {
+import java.util.List;
 
+public interface MessageRepository extends CrudRepository<Invite, Long>, PagingAndSortingRepository<Invite, Long> {
+    List<Message> findByChat(Chat chat);
 }
