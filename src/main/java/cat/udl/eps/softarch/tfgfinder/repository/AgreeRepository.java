78ced0f8d8542f7bb86bae6bf3232716439458f4
Cesar2306 <torcapolo2@gmail.com>
2025-03-13 17:28:45 +0100

    Changed the what type in Agree and Agree Repository

--- src/main/java/cat/udl/eps/softarch/tfgfinder/repository/AgreeRepository.java
+++ src/main/java/cat/udl/eps/softarch/tfgfinder/repository/AgreeRepository.java
@@ -1,6 +1,7 @@
 package cat.udl.eps.softarch.tfgfinder.repository;
 
 import cat.udl.eps.softarch.tfgfinder.domain.Agree;
+import cat.udl.eps.softarch.tfgfinder.domain.Proposal;
 import cat.udl.eps.softarch.tfgfinder.domain.User;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.PagingAndSortingRepository;
@@ -11,5 +12,5 @@ import java.util.List;
 @RepositoryRestResource
 public interface AgreeRepository extends CrudRepository<Agree, Long >, PagingAndSortingRepository<Agree, Long > {
     List<Agree> findByWho(User who);
-    List<Agree> findByWhat(User what);
+    List<Agree> findByWhat(Proposal what);
 }
