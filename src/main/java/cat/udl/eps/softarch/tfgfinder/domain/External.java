292c1c217ec7ab55053c5473a4c2b9ed91c5f72c
carless7 <csh3@alumnes.udl.cat>
2025-03-26 18:56:38 +0100

    feat: added some params to external

--- src/main/java/cat/udl/eps/softarch/tfgfinder/domain/External.java
+++ src/main/java/cat/udl/eps/softarch/tfgfinder/domain/External.java
@@ -2,6 +2,8 @@ package cat.udl.eps.softarch.tfgfinder.domain;
 
 import jakarta.persistence.ElementCollection;
 import jakarta.persistence.Entity;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import org.springframework.security.core.GrantedAuthority;
@@ -13,9 +15,13 @@ import java.util.Collection;
 @Data
 @EqualsAndHashCode(callSuper = true)
 public class External extends Director {
+
+    @NotBlank
+    private String companyName;
+
     @Override
     @ElementCollection
-    public Collection<GrantedAuthority> getAuthorities(){
+    public Collection<GrantedAuthority> getAuthorities() {
         return AuthorityUtils.commaSeparatedStringToAuthorityList("ROLE_EXTERNAL");
     }
 }
