8cbe7c46ae75470e2d8034c62ee380f754b41e04
carless7 <csh3@alumnes.udl.cat>
2025-03-26 18:42:47 +0100

    feat: typo fixes

--- src/main/java/cat/udl/eps/softarch/tfgfinder/repository/ProfessorRepository.java
+++ src/main/java/cat/udl/eps/softarch/tfgfinder/repository/ProfessorRepository.java
@@ -11,13 +11,11 @@ import java.util.List;
 @RepositoryRestResource
 public interface ProfessorRepository extends CrudRepository<Professor, String>, PagingAndSortingRepository<Professor, String> {
 
-    // Custom query methods for Professor-specific fields
     List<Professor> findByDepartment(@Param("department") String department);
 
     List<Professor> findByCenter(@Param("center") String center);
 
     List<Professor> findByOffice(@Param("office") String office);
 
-    // Combined query example
     List<Professor> findByDepartmentAndCenter(@Param("department") String department, @Param("center") String center);
 }
