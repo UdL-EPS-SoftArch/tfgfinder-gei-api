cdae68196cb67588f7d49390549f191dbf242a13
Aleix-CB <aleix.cerqueda@gmail.com>
2025-05-17 13:36:24 +0200

    Proposal Inicialization For testing

--- src/main/java/cat/udl/eps/softarch/tfgfinder/config/DBInitialization.java
+++ src/main/java/cat/udl/eps/softarch/tfgfinder/config/DBInitialization.java
@@ -1,5 +1,7 @@
 package cat.udl.eps.softarch.tfgfinder.config;
+import cat.udl.eps.softarch.tfgfinder.domain.Proposal;
 import cat.udl.eps.softarch.tfgfinder.domain.User;
+import cat.udl.eps.softarch.tfgfinder.repository.ProposalRepository;
 import cat.udl.eps.softarch.tfgfinder.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
@@ -13,14 +15,21 @@ public class DBInitialization {
     @Value("${spring.profiles.active:}")
     private String activeProfiles;
     private final UserRepository userRepository;
+    private final ProposalRepository proposalRepository;
 
-    public DBInitialization(UserRepository userRepository) {
+    public DBInitialization(UserRepository userRepository, ProposalRepository proposalRepository) {
         this.userRepository = userRepository;
+        this.proposalRepository = proposalRepository;
     }
 
     @PostConstruct
     public void initializeDatabase() {
+
+
+
+
         // Default user
+
         if (!userRepository.existsById("demo")) {
             User user = new User();
             user.setEmail("demo@sample.app");
@@ -28,6 +37,17 @@ public class DBInitialization {
             user.setPassword(defaultPassword);
             user.encodePassword();
             userRepository.save(user);
+        }
+        if (proposalRepository.findByTitle("ZTNA") != null) {
+            Proposal proposal = new Proposal();
+            proposal.setTitle("ZTNA");
+            proposal.setDescription("ZTNA");
+            proposal.setTiming("Q1 2025");
+            proposal.setSpeciality("Cybersecurity");
+            proposal.setKind("Master Thesis");
+            proposal.setKeywords("ZTNA, security, networking");
+            proposal.setOwner(userRepository.findById("demo").get());
+            proposalRepository.save(proposal);
         }
         if (Arrays.asList(activeProfiles.split(",")).contains("test")) {
             // Testing instances
@@ -40,5 +60,7 @@ public class DBInitialization {
                 userRepository.save(user);
             }
         }
+
+
     }
 }
